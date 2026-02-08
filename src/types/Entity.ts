082a3a54351d55f8289154f1a728f9ac984d9fc6
Jake Gibellini <jake@pixellini.com>
2026-02-08 14:59:50 +1100

    chore: remove old entity type

--- src/types/Entity.ts
+++
@@ -1,6 +0,0 @@
-import type { Container, DestroyOptions } from 'pixi.js'
-
-export interface Entity extends Container {
-    onResize(width: number, height: number): void
-    destroy(options?: DestroyOptions): void
-}
